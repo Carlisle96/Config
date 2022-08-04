206d8f307ab8ae293bdced636cb3d83e695c52a1
Carlisle Nightingale <thyriaen@googlemail.com>
2022-08-04 12:19:43 +0200

    blubbi

---
+++ get.sh
@@ -0,0 +1,15 @@
+
+cp ~/.local/share/applications/asana.desktop .
+cp ~/.local/share/applications/darlehen.desktop .
+cp ~/.local/share/applications/Datev.desktop .
+cp ~/.local/share/applications/gdocs.desktop .
+cp ~/.local/share/applications/spendesk.desktop .
+cp ~/.local/share/applications/superhuman.desktop .
+
+cp /usr/share/X11/xkb/symbols/thy .
+cp /usr/share/X11/xkb/rules/evdev.xml .
+
+cp ~/Pictures/Wallpaper.jpg .
+
+cp ~/.config/watson/frames .
+cp ~/.config/alacritty/alacritty.yml .
