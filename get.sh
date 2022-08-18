779d496c2df753ef9785d8ffdaeea5dc4f5b3467
Carlisle Nightingale <thyriaen@googlemail.com>
2022-08-18 14:21:16 +0200

    zathura bg

--- get.sh
+++ get.sh
@@ -4,6 +4,7 @@ cp ~/.profile ./home/profile
 cp ~/.zshrc ./home/zshrc
 
 cp ~/.config/nnn/plugins/drag ./cfg/nnn/plugins/
+cp ~/.config/zathura/zathurarc ./cfg/zathura/
 
 cp ~/.config/sublime-text/Packages/User/Preferences.sublime-settings ./cfg/sublime/
 cp ~/.config/sublime-text/Packages/User/Monokai\ Thyriaen.sublime-color-scheme ./cfg/sublime/
