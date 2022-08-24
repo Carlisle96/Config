5d6ed15728d986b01b660939b62631b7ec4350f8
Carlisle Nightingale <thyriaen@gmail.com>
2022-08-24 05:44:16 -0400

    filechooser works

--- get.sh
+++ get.sh
@@ -2,7 +2,6 @@ cp ~/.config/kitty/kitty.conf ./cfg/kitty/
 cp ~/.xmonad/xmonad.hs ./xmonad/
 cp ~/.profile ./home/profile
 cp ~/.zshrc ./home/zshrc
-cp ~/.xmobarrc ./home/xmobarrc
 
 cp ~/.config/nnn/plugins/drag ./cfg/nnn/plugins/
 cp ~/.config/zathura/zathurarc ./cfg/zathura/
@@ -10,3 +9,9 @@ cp ~/.config/zathura/zathurarc ./cfg/zathura/
 cp ~/.config/sublime-text/Packages/User/Preferences.sublime-settings ./cfg/sublime/
 cp ~/.config/sublime-text/Packages/User/Monokai\ Thyriaen.sublime-color-scheme ./cfg/sublime/
 cp ~/.config/sublime-text/Packages/User/Package\ Control.sublime-settings ./cfg/sublime/
+
+cp /etc/systemd/system/xdg-desktop-portal-termfilechooser.service ./cfg/nnn/filechooser/
+cp /usr/share/xdg-desktop-portal/portals/termfilechooser.portal ./cfg/nnn/filechooser/
+cp ~/.local/bin/xdg-desktop-portal-termfilechooser ./cfg/nnn/filechooser/
+cp ~/.config/xdg-desktop-portal-termfilechooser/config ./cfg/nnn/filechooser/
+cp ~/.config/xdg-desktop-portal-termfilechooser/fff.sh ./cfg/nnn/filechooser/
