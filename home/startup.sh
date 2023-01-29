b0e6b05cb9fcc3902276624a3f0f93e08443322e
Carlisle Nightingale <thyriaen@gmail.com>
2023-01-29 01:19:56 +0100

    new startup

--- home/startup.sh
+++ home/startup.sh
@@ -1,4 +1,4 @@
-feh --no-fehbg --bg-fill '/home/thyriaen/Pictures/Wallpapers/purpleMoonDesktop2.png' '/home/thyriaen/Pictures/Wallpapers/purpleMoonDesktopRight.png'
+feh --no-fehbg --bg-fill '/home/thyriaen/Pictures/Wallpapers/purpleMoonDesktop3.png' '/home/thyriaen/Pictures/Wallpapers/purpleMoonDesktopRight3.png'
 xinput --set-prop 'pointer:Logitech G900' 165 1.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000, 0.000000, 3.000000
 xsetroot -cursor_name left_ptr
 setxkbmap -layout thy
