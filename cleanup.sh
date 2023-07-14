fb3072e610ba6d5f3e942bdc8327c7dac75e4b1f
Carlisle Nightingale <thyriaen@googlemail.com>
2023-07-14 11:45:13 +0200

    fstab fix

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,6 @@
 rm ~/.bash*
 sudo dnf -y remove i3 awesome ratpoison openbox
+
+fstabstring="
+UUID=75868785-af97-4b46-a511-1b7122e54953 /home/thyriaen/Data	   ext4	 defaults 	0 0
+UUID=3baa35ef-d65e-435a-827b-e6166993d36e /home/thyriaen/Documents ext4	 defaults 	0 0"
