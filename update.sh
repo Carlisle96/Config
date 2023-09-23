6de49a76bf40cfe125e7be35c2e769118d12df67
Carlisle Nightingale <thyriaen@googlemail.com>
2023-09-23 17:57:52 +0200

    fresh start

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
-cp ~/.xmonad/xmonad.hs ./cfg/xmonad/
+cp ~/.config/xmonad/xmonad.hs ./cfg/xmonad/
 cp ~/.config/dunst/dunstrc ./cfg/dunst/
-cp ~/.config/tint2/* ./cfg/tint2/
+cp -r ~/.config/tint2/* ./cfg/tint2/
 cp ~/.config/xmonad/hooks/tint.sh ./cfg/xmonad/hooks/
 cp ~/.config/mimeapps.list ./cfg/
 cp ~/.config/picom/picom.conf ./cfg/picom/
