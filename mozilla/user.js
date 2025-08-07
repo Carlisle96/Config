4cff07e26e9effad28bfa8f8f025131f5edfe427
Carlisle Nightingale <thyriaen@googlemail.com>
2025-08-07 16:44:40 +0200

    added mozilla

---
+++ mozilla/user.js
@@ -0,0 +1,11 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Fill SVG Color
+user_pref("svg.context-properties.content.enabled", true);
+
+// Trim  URL
+user_pref("browser.urlbar.trimHttps", true);
+user_pref("browser.urlbar.trimURLs", true);
+
+// GTK rounded corners
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
