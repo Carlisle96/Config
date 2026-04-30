bac0dd13a81dc361d971cda53368cceb321d1bf2
Carlisle Nightingale <thyriaen@googlemail.com>
2026-04-30 19:48:39 +0200

    Move custom XKB layout to dedicated path

--- setup.sh
+++ setup.sh
@@ -3,23 +3,22 @@
 ### ------------------------------------ Variables ----------------------------------- ###
 
 PACKAGES="flatpak pdftk python3-pip zathura zathura-pdf-mupdf bat imv task
-kitty zsh zsh-syntax-highlighting fzf fastfetch mesa-libOpenCL clinfo
-evince simple-scan hexchat keepassxc mate-calc syncthing mediawriter nemo
-gtk-murrine-engine gtk3-devel remmina fuse fuse-libs cups cups-filters
-darktable brightnessctl dunst"
+kitty zsh zsh-syntax-highlighting fzf fastfetch mesa-libOpenCL clinfo evince
+simple-scan keepassxc mate-calc syncthing mediawriter nemo dunst brightnessctl
+gtk-murrine-engine gtk3-devel fuse fuse-libs cups cups-filters pavucontrol"
 
-HYPRLAND="hyprland hyprpaper sddm hyprland-devel pavucontrol firefox
-rofi-wayland wlsunset xdg-desktop-portal-hyprland"
+HYPRLAND="hyprland hyprpaper sddm hyprland-devel rofi-wayland wlsunset
+xdg-desktop-portal-hyprland"
 
 HYPRPM="cmake meson gcc-c++ hyprlang-devel hyprcursor-devel mesa-libgbm-devel libdrm-devel
 mesa-libGLES-devel hyprutils-devel aquamarine-devel wayland-devel pango-devel
 hyprgraphics-devel tomlplusplus-devel systemd-devel socat cairo-devel pixman-devel
 glib2-devel re2-devel libinput-devel libxkbcommon-devel libuuid-devel libXcursor-devel
 xcb-util-errors-devel wayland-protocols-devel udis86-devel hyprwayland-scanner-devel
-xcb-util-wm-devel"
+xcb-util-wm-devel muParser-devel hyprwire-devel"
 
 SDDMTHEME="qt6-qt5compat qt5-qtgraphicaleffects qt5-qtquickcontrols2"
-OFFICE="libreoffice-calc libreoffice-gtk3"
+OFFICE="libreoffice-calc libreoffice-gtk3 darktable web-eid hexchat firefox"
 
 LATEX="texlive-scheme-basic latexmk texlive-bibtex8 texlive-standalone texlive-preview
 texlive-mathtools texlive-babel-german texlive-multirow texlive-eurosym texlive-spreadtab
@@ -28,7 +27,7 @@ texlive-datetime2-german texlive-hyphen-german texlive-xskak texlive-skak texliv
 texlive-collection-fontsrecommended texlive-doi texlive-mdframed texlive-fontawesome5
 texlive-ebgaramond texlive-datetime2-english"
 
-EXTERNAL="google-chrome-stable sublime-text synology-drive-noextra web-eid"
+EXTERNAL="google-chrome-stable sublime-text synology-drive-noextra"
 
 DESKTOP="easyeffects"
 LAPTOP="tlp light"
@@ -50,7 +49,6 @@ fi
 # Official Fedora repos ship Hyprland but lag behind -- COPR ensures latest version
 sudo dnf -y copr enable sdegler/hyprland
 sudo dnf -y copr enable emixampp/synology-drive
-sudo dnf -y copr enable abn/web-eid
 
 # Sublime Text
 sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
@@ -103,14 +101,12 @@ sudo dnf -y install "$POMOTROID_URL"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Global theme overrides
-sudo flatpak override --filesystem=$HOME/.themes
-sudo flatpak override --filesystem=$HOME/.icons
+sudo flatpak override --filesystem=$HOME
 sudo flatpak override --env=GTK_THEME=mathy
 sudo flatpak override --env=ICON_THEME=Newaita-reborn-deep-purple-dark
 
 # Signal
 sudo flatpak install -y flathub org.signal.Signal
-sudo flatpak override --user --filesystem=home org.signal.Signal
 sudo flatpak override --user --env=PULSE_LATENCY_MSEC=30 org.signal.Signal
 sudo flatpak override --user --env=ELECTRON_OZONE_PLATFORM_HINT=auto org.signal.Signal
 
@@ -191,6 +187,11 @@ cp -r ./localbin/* ~/.local/bin/
 
 # Global configs
 sudo cp -r ./usrshare/* /usr/share/
+
+# Custom XKB layout -- /etc/xkb/ survives xkeyboard-config package updates
+sudo mkdir -p /etc/xkb/symbols /etc/xkb/rules
+sudo cp ./xkb/symbols/thy /etc/xkb/symbols/thy
+sudo cp ./xkb/rules/evdev.xml /etc/xkb/rules/evdev.xml
 
 # User themes and icons
 mkdir -p ~/.themes ~/.icons
